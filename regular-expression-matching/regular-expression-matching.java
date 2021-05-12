00ba03e9d9301e74dcbc4d54239b387059b7b559
Han Weng <36247722+Han-Weng@users.noreply.github.com>
2021-05-12 10:54:10 -0400

    Time: 4 ms (52.24%), Space: 39.1 MB (33.51%) - LeetHub

--- regular-expression-matching/regular-expression-matching.java
+++ regular-expression-matching/regular-expression-matching.java
@@ -1,33 +1,33 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
-        if(s == null || p == null) {
+
+        if (s == null || p == null) {
             return false;
         }
-        boolean[][] state = new boolean[s.length() + 1][p.length() + 1];
-        state[0][0] = true;
-        // no need to initialize state[i][0] as false
-        // initialize state[0][j]
-        for (int j = 1; j < state[0].length; j++) {
-            if (p.charAt(j - 1) == '*') {
-                if (state[0][j - 1] || (j > 1 && state[0][j - 2])) {
-                    state[0][j] = true;
-                }
-            } 
+        boolean[][] dp = new boolean[s.length()+1][p.length()+1];
+        dp[0][0] = true;
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '*' && dp[0][i-1]) {
+                dp[0][i+1] = true;
+            }
         }
-        for (int i = 1; i < state.length; i++) {
-            for (int j = 1; j < state[0].length; j++) {
-                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
-                    state[i][j] = state[i - 1][j - 1];
+        for (int i = 0 ; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) == '.') {
+                    dp[i+1][j+1] = dp[i][j];
                 }
-                if (p.charAt(j - 1) == '*') {
-                    if (s.charAt(i - 1) != p.charAt(j - 2) && p.charAt(j - 2) != '.') {
-                        state[i][j] = state[i][j - 2];
+                if (p.charAt(j) == s.charAt(i)) {
+                    dp[i+1][j+1] = dp[i][j];
+                }
+                if (p.charAt(j) == '*') {
+                    if (p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.') {
+                        dp[i+1][j+1] = dp[i+1][j-1];
                     } else {
-                        state[i][j] = state[i - 1][j] || state[i][j - 1] || state[i][j - 2];
+                        dp[i+1][j+1] = (dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1]);
                     }
                 }
             }
         }
-        return state[s.length()][p.length()];
+        return dp[s.length()][p.length()];
     }
 }
