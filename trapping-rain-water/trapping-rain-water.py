11b721abb556bf2a122926f684441237f26d9753
Han Weng <36247722+Han-Weng@users.noreply.github.com>
2021-06-02 23:49:57 -0400

    Time: 108 ms (9.04%), Space: 15.3 MB (7.26%) - LeetHub

---
+++ trapping-rain-water/trapping-rain-water.py
@@ -0,0 +1,26 @@
+class Solution:
+        def trap(self, height: List[int]) -> int:
+            rightList = []
+            rightBig = 0
+            leftList = []
+            leftBig = 0 
+            
+            for l in height:
+                leftBig = max(l, leftBig)
+                leftList.append(leftBig)
+            
+            reverse =  height[::-1]
+            for r in reverse:
+                rightBig = max(r, rightBig)
+                rightList.append(rightBig)
+            rightList = rightList[::-1]
+            trapped= 0
+            
+            i = 0
+            for h in reverse:
+                
+                trapped += min(leftList[i], rightList[i]) - h
+                i += 1
+        
+            return trapped
+        
