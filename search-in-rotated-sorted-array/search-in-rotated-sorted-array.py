288158181be4713b75ed687b2028214daaa6233d
H <36247722+Han-Weng@users.noreply.github.com>
2021-07-16 16:59:38 -0400

    Time: 44 ms (51.26%), Space: 14.5 MB (79.73%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -5,18 +5,13 @@ class Solution:
     def search(self, nums, target):
         if not nums:
             return -1
-
         low, high = 0, len(nums) - 1
 
         while low <= high:
             mid = (low + high) // 2
-            print(low)
-            print(high)
-            print(mid)
-            print('=======================')
             if target == nums[mid]:
                 return mid
-
+            
             if nums[low] <= nums[mid]:
                 if nums[low] <= target <= nums[mid]:
                     high = mid - 1
@@ -27,6 +22,5 @@ class Solution:
                     low = mid + 1
                 else:
                     high = mid - 1
-            
 
         return -1
