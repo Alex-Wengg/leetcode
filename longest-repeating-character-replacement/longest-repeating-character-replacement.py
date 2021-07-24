817758ecc851ff9e6c5355b6cfaf31800862cbda
H <36247722+Han-Weng@users.noreply.github.com>
2021-07-23 21:10:01 -0400

    Time: 88 ms (96.74%), Space: 14.3 MB (81.07%) - LeetHub

---
+++ longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,25 @@
+class Solution:
+    def characterReplacement(self, str1: str, k: int) -> int:
+        mapp = {}
+        largest = 0
+        result = 0
+        start = 0 
+        # try to extend the range [windowStart, windowEnd]
+        for end in range(len(str1)):
+            right = str1[end]
+
+
+            if right not in mapp:
+                mapp[right] = 0
+            mapp[right] += 1 
+            
+            largest = max(largest, mapp[right])
+            
+            if(end - start + 1 - largest) > k:
+                left = str1[start]
+                mapp[left] -= 1 
+                start += 1 
+            
+    
+
+        return end - start + 1
