8f2259ebd383c19aece9650cdce140b60c6c48f4
H <36247722+Han-Weng@users.noreply.github.com>
2021-08-01 17:42:52 -0400

    Time: 40 ms (43.29%), Space: 14.7 MB (19.79%) - LeetHub

--- different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
+++ different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
@@ -1,25 +1,23 @@
 class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
-        
         result = []
-        # base case: if the input string is a number, parse and add it to output.
-        if '+' not in expression and '-' not in expression and '*' not in expression:
+        
+        
+        if "-" not in expression and "+" not in expression and "*" not in expression:
             result.append(int(expression))
         else:
-            for i in range(0, len(expression)):
-                char = expression[i]
-                if not char.isdigit():
-                    # break the equation here into two parts and make recursively calls
-                    leftParts = self.diffWaysToCompute(expression[0:i])
-                    rightParts = self.diffWaysToCompute(expression[i+1:])
-                    for part1 in leftParts:
-                        for part2 in rightParts:
-                            if char == '+':
-                                result.append(part1 + part2)
-                            elif char == '-':
-                                result.append(part1 - part2)
-                            elif char == '*':
-                                result.append(part1 * part2)
-                                    
-                                    
-        return  result
+            for i in range(len(expression)):
+                print(i)
+                if expression[i].isdigit() ==False:
+                    part1 = self.diffWaysToCompute(expression[0:i])
+                    part2 = self.diffWaysToCompute(expression[i+1:len(expression)])
+
+                    for a in part1:
+                        for b in part2:
+                            if  expression[i] == '+':
+                                result.append(int(a) + int(b))
+                            if  expression[i] == '*':
+                                result.append(int(a) * int(b))
+                            if  expression[i] == '-':
+                                result.append(int(a) - int(b))
+        return result
