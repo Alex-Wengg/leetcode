59217d35b979f1e0540d91746ffa5ea7d11827c3
H <36247722+Han-Weng@users.noreply.github.com>
2021-08-01 14:33:36 -0400

    Time: 36 ms (64.19%), Space: 14.6 MB (39.76%) - LeetHub

--- generate-parentheses/generate-parentheses.py
+++ generate-parentheses/generate-parentheses.py
@@ -1,9 +1,17 @@
+  
 class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
 
-        def generate(p, left, right, parens=[]):
-            if left:         generate(p + '(', left-1, right)
-            if right > left: generate(p + ')', left, right-1)
-            if not right:    parens += p,
-            return parens
-        return generate('', n, n)
+            
+    def generateParenthesis(self, n: int) -> List[str]:
+        def helper(result, open, close, nums, element):
+            if len(element) == nums*2:
+                result.append(element)
+            if open < nums:
+                helper(result, open+1, close, nums, element+"(")
+            if open > close:
+                helper(result, open, close+1, nums, element+")")    
+                
+        result = []
+        helper(result,0,0,n, "")
+        return result
+    
