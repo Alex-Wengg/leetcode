fbfcc6c8cdeea5e8bc1bdac7f7e8169f496adcff
H <36247722+Han-Weng@users.noreply.github.com>
2021-08-02 15:58:39 -0400

    Time: 500 ms (18.49%), Space: 14.5 MB (77.03%) - LeetHub

--- target-sum/target-sum.py
+++ target-sum/target-sum.py
@@ -1,12 +1,28 @@
 class Solution:
-    def findTargetSumWays(self, nums, S):
-        count =  defaultdict(int)
-        count[0] = 1
-        for x in nums:
-            step = defaultdict(int)
-            for y in count:
-                step[y + x] += count[y]
-                step[y - x] += count[y]
-            count = step
-        print(count)
-        return count[S]
+    def findTargetSumWays(self, nums, s):
+        summ = 0
+        for i in nums:
+            summ +=i
+        
+        if (s > summ or s< -summ):
+            return 0 
+        
+        dp = []
+        for i in range(2*summ+1):
+            dp.append(0) 
+        dp[0+summ] = 1;
+        
+        
+        for i in range(len(nums)):
+            nextt = []
+            for x in range(2*summ+1):
+                nextt.append(int(0)) 
+
+            for k in range(2*summ+1):
+                
+                if (dp[k] != 0):
+                    nextt[k + nums[i]] += dp[k]
+                    nextt[k - nums[i]] += dp[k]
+            dp = nextt
+        
+        return dp[summ+s]
