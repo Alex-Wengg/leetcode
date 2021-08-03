0a1dc7d9d4614b2db01ec18f812e6d5f41e35cf7
H <36247722+Han-Weng@users.noreply.github.com>
2021-08-03 10:25:37 -0400

    Time: 252 ms (70.46%), Space: 14.4 MB (77.03%) - LeetHub

--- target-sum/target-sum.py
+++ target-sum/target-sum.py
@@ -1,28 +1,12 @@
 class Solution:
-    def findTargetSumWays(self, nums, s):
-        summ = 0
-        for i in nums:
-            summ +=i
-        
-        if (s > summ or s< -summ):
-            return 0 
-        
-        dp = []
-        for i in range(2*summ+1):
-            dp.append(0) 
-        dp[0+summ] = 1;
-        
-        
-        for i in range(len(nums)):
-            nextt = []
-            for x in range(2*summ+1):
-                nextt.append(int(0)) 
-
-            for k in range(2*summ+1):
-                
-                if (dp[k] != 0):
-                    nextt[k + nums[i]] += dp[k]
-                    nextt[k - nums[i]] += dp[k]
-            dp = nextt
-        
-        return dp[summ+s]
+    def findTargetSumWays(self, nums, S):
+        if not nums:
+            return 0
+        dic = {nums[0]: 1, -nums[0]: 1} if nums[0] != 0 else {0: 2}
+        for i in range(1, len(nums)):
+            tdic = {}
+            for d in dic:
+                tdic[d + nums[i]] = tdic.get(d + nums[i], 0) + dic.get(d, 0)
+                tdic[d - nums[i]] = tdic.get(d - nums[i], 0) + dic.get(d, 0)
+            dic = tdic
+        return dic.get(S, 0)
