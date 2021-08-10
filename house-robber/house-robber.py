a993154326ea6862a4afd46081aa487bebf8d7cd
H <36247722+Han-Weng@users.noreply.github.com>
2021-08-10 15:30:22 -0400

    Time: 20 ms (99.24%), Space: 14.1 MB (92.85%) - LeetHub

---
+++ house-robber/house-robber.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        prev = 0
+        curr = 0
+        
+        for num in nums:
+            prev, curr = curr, max(curr, prev + num)
+
+       
+            
+            
+        return curr
