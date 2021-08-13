d8ddeaa5edf04f585aee0a47350b097fdad7b453
H <36247722+Han-Weng@users.noreply.github.com>
2021-08-13 16:25:10 -0400

    Time: 84 ms (96.44%), Space: 14.9 MB (29.50%) - LeetHub

--- minimum-window-substring/minimum-window-substring.py
+++ minimum-window-substring/minimum-window-substring.py
@@ -1,36 +1,37 @@
 class Solution:
     def minWindow(self, str1: str, pattern: str) -> str:
-    
-    
-        start, match, substr_start = 0, 0, 0
-        substr = { 'start' : 0, 'end' :  len(str1) }
-        mapp = {}
+        hashh = {}
+        for i in pattern:
+            if i not in hashh:
+                hashh[i] = 0
+            hashh[i] += 1 
             
-        for chr in pattern:
-            if chr not in mapp:
-                mapp[chr] = 0
-            mapp[chr] += 1
-
-        # try to extend the range [window_start, window_end]
+        start = 0 
+        match = 0
+        head = 0
+        tail = len(str1)
         for end in range(len(str1)):
-            right  = str1[end]
-            if right in mapp:
-                mapp[right] -= 1
-                if mapp[right] >= 0:
-                    match += 1
+            right = str1[end]
+            if right in hashh:
+                hashh[right] -= 1
+                if hashh[right] >= 0:
+                    match += 1 
+                    
+
             
-            while len(pattern) == match:
+            while match == len(pattern):
+                if tail- head +1 >  end - start +1:
+                    head = start
+                    tail = end
                 
-                if (substr['end'] - substr['start'] + 1) > end - start + 1:
-                    substr['end'] = end 
-                    substr['start'] = start
+                left = str1[start]
                 
-                left = str1[start] 
-                if left in mapp: 
-                    if mapp[left] == 0:
-                        match -=1 
-                    mapp[left] += 1
+                if left in hashh:
+                   
+                    if hashh[left] == 0:
+                        match -= 1    
+                    hashh[left] += 1
                 start += 1 
-        if substr['end'] ==  len(str1):
+        if tail ==  len(str1):
             return ""
-        return str1[substr['start']:(substr['end']+1)]
+        return str1[head:tail+1]
