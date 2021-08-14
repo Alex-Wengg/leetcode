92e6318392c04fb7ba079aec72de7ad5bec0d100
H <36247722+Han-Weng@users.noreply.github.com>
2021-08-14 14:52:25 -0400

    Time: 968 ms (73.79%), Space: 14.4 MB (39.18%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.py
+++ longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,22 +1,20 @@
-        
-def middle(s, left,right):
-    while(0<=left and right< len(s) and s[left] == s[right]):
-        left -=1
-        right +=1
-    return (right - left -1)
+def middle(s, left ,right):
+    while 0 <= left and right < len(s)  and s[right] == s[left]:
+        left -= 1 
+        right += 1
+    return right - left -  1
 
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
+    def longestPalindrome(self, s: str) -> str:        
         start = 0
-        end = 0
+        end  = 0 
         for i in range(len(s)):
-            len1 = middle(s, i, i)
-            len2 = middle(s, i, i+1)
-            bestLen = max(len1,len2)
+            even = middle(s, i, i)
+            odd = middle(s,i, i+1)
+            best = max(even, odd)
             
-            if (bestLen > end-start):
-                start = i - (bestLen-1)//2
-                end = i + bestLen//2
-        
+            if best > end-start:
+                start = i - (best-1)//2
+                end = i + best//2
+            
         return s[start:end+1]
-                
