5a2c473a8e70d7eb85c74327e4d7caf55ace2473
H <36247722+Han-Weng@users.noreply.github.com>
2021-09-18 14:39:04 -0700

    Time: 48 ms (86.00%), Space: 14.5 MB (98.01%) - LeetHub

--- analyze-user-website-visit-pattern/analyze-user-website-visit-pattern.py
+++ analyze-user-website-visit-pattern/analyze-user-website-visit-pattern.py
@@ -12,26 +12,20 @@ class Solution:
         # o(n)
         for i in range(len(sorted_values)):
             stamp, name, site = sorted_values.pop(0)
-            if name in users:
-                users[name].append(site)
-            else:
-                users[name] = [site]
+            users[name] = users.get(name, [])  + [site]
         
         frequency = {}
 		#o(n^3)
         for user, sites in users.items():
-            if len(sites) < 3:
-                continue
             seen = set()
             for i in range(len(sites)):
                 for j in range(i + 1, len(sites)):
                     for k in range(j + 1, len(sites)):
                         seen.add((sites[i], sites[j], sites[k]))
             for pattern in seen:
-                if pattern not in frequency:
-                    frequency[pattern] = 0
-                frequency[pattern] += 1
-                
+                frequency[pattern] = frequency.get(pattern, 0) +1  
+
+
 		#o(n)
         return min(frequency, key=lambda x: (-frequency[x], x))
 	
