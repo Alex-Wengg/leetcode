f79790ca301612f64de67343b2dc61e4612f78a5
H <36247722+Han-Weng@users.noreply.github.com>
2021-09-20 19:09:27 -0700

    Time: 107 ms (6.64%), Space: 14.6 MB (80.07%) - LeetHub

--- analyze-user-website-visit-pattern/analyze-user-website-visit-pattern.py
+++ analyze-user-website-visit-pattern/analyze-user-website-visit-pattern.py
@@ -5,27 +5,32 @@ class Solution:
         sorted_values = []
         
         # nlog(n)
+        #package the data and sort them by time
         for name, stamp, site in zip(username, timestamp, website):
             sorted_values.append([stamp, name, site])
         sorted_values.sort(key = lambda x :x[0])
 
         # o(n)
+        #track the user & websites they visited, oldest to recent
         for i in range(len(sorted_values)):
             stamp, name, site = sorted_values.pop(0)
             users[name] = users.get(name, [])  + [site]
         
         frequency = {}
 		#o(n^3)
+        #for each user, loop their site travels
         for user, sites in users.items():
             seen = set()
             for i in range(len(sites)):
                 for j in range(i + 1, len(sites)):
                     for k in range(j + 1, len(sites)):
                         seen.add((sites[i], sites[j], sites[k]))
+            #site list and frequency
             for pattern in seen:
                 frequency[pattern] = frequency.get(pattern, 0) +1  
 
 
 		#o(n)
+        #min(list, ranked by frequency using lambda)
         return min(frequency, key=lambda x: (-frequency[x], x))
 	
