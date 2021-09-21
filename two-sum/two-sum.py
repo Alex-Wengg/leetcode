d5275b2e7b1d22f1e7c89cf9001e6911e281f256
H <36247722+Han-Weng@users.noreply.github.com>
2021-09-20 17:13:30 -0700

    Time: 85 ms (48.92%), Space: 15.6 MB (9.62%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,9 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-            newHash={}
-            for count, num in enumerate(nums):
-
-                if num not in newHash:
-                    newHash[target-num] = count
-                else:
-                    return [newHash[num], count]
+        dic = {}
+        for i, num in enumerate(nums):
+            if num in dic:
+                return [dic[num],i]
+            else:
+                dic[target - num]  = i 
