075b91102bca53c09993594a8cdb2e7db09e2264
H <36247722+Han-Weng@users.noreply.github.com>
2021-09-20 17:01:52 -0700

    Time: 36 ms (66.91%), Space: 14.3 MB (75.28%) - LeetHub

--- design-an-expression-tree-with-evaluate-function/design-an-expression-tree-with-evaluate-function.py
+++ design-an-expression-tree-with-evaluate-function/design-an-expression-tree-with-evaluate-function.py
@@ -1,3 +1,4 @@
+# Polymorphism concept taken from https://stackoverflow.com/questions/12516/expression-evaluation-and-tree-walking-using-polymorphism-ala-steve-yegge
 import abc 
 from abc import ABC, abstractmethod 
 """
@@ -11,53 +12,44 @@ class Node(ABC):
     def evaluate(self) -> int:
         pass
 
-class BinaryNode(Node):
-    """base class for binary nodes"""
-    def __init__(self, _left, _right):
-        self.left = _left
-        self.right = _right
-    def evaluate(self) -> int:
-        pass
-
-
-class Plus(BinaryNode):
-    def evaluate(self) -> int:
-        return self.left.evaluate() + self.right.evaluate()
-
-class Minus(BinaryNode):
-    def evaluate(self) -> int:
-        return self.left.evaluate() - self.right.evaluate()
-
-class Mul(BinaryNode):
-    def evaluate(self) -> int:
-        return self.left.evaluate() * self.right.evaluate()
-
-class Div(BinaryNode):
-    def evaluate(self) -> int:
-        return self.left.evaluate() // self.right.evaluate()
-
-class Num(Node):
-    def __init__(self, _value):
-        self.value = _value
-    def evaluate(self) -> int:
-        return self.value
-
+class TreeNode(Node):
+    def __init__(self, value, op=None):
+        self.right = None
+        self.left = None
+        self.op = op
+        self.value  = value
+        
+    def evaluate(self):
+        
+        if None == self.op:
+            return int(self.value)
+        if self.op == "+":
+            return self.left.evaluate() + self.right.evaluate()
+        if self.op == "-":
+            return self.left.evaluate() - self.right.evaluate()
+        if self.op == "*":
+            return self.left.evaluate() * self.right.evaluate()
+        if self.op == "/":
+            return self.left.evaluate() // self.right.evaluate()        
+        
 """    
 This is the TreeBuilder class.
 You can treat it as the driver code that takes the postinfix input
 and returns the expression tree represnting it as a Node.
 """
 
-class TreeBuilder(object):
+class  TreeBuilder(object):
     def buildTree(self, postfix: List[str]) -> 'Node':
-        operators = {'+': Plus, '-': Minus, '*': Mul, '/': Div}
-        stk = []
-        for token in postfix:
-            if token in operators:
-                R = stk.pop()
-                L = stk.pop()
-                stk.append(operators[token](L, R))
+    
+        ops = set(['+', '-', '*', '/'])
+        stack = []
+        for value in postfix:
+            if value in ops:
+                operate = TreeNode(0,value)
+                operate.right = stack.pop()
+                operate.left = stack.pop()
+                stack.append(operate)
             else:
-                stk.append(Num(int(token)))
-        return stk[0]
-		
+                operate = TreeNode(value)
+                stack.append(operate)
+        return stack[-1]
