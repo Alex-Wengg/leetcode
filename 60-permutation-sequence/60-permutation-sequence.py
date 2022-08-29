8626f4d089449139fc5e378c5398c0384c5492e9
H <36247722+Han-Weng@users.noreply.github.com>
2022-08-28 21:23:50 -0400

    Time: 28 ms (98.15%), Space: 13.9 MB (79.03%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.py
+++ 60-permutation-sequence/60-permutation-sequence.py
@@ -4,9 +4,10 @@ class Solution:
         nums = list(map(str, range(1, 10)))
         res = []
         k -= 1
-        
         while n > 0:
+            
             n -= 1
+            
             index, k = divmod(k, math.factorial(n))
             res.append(nums[index])
             nums.remove(nums[index])
