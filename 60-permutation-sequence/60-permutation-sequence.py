db6ae0054b8b49ab250bca3c3c21b3db19e58fff
H <36247722+Han-Weng@users.noreply.github.com>
2022-08-28 21:22:44 -0400

    Time: 36 ms (87.92%), Space: 13.9 MB (32.53%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.py
+++ 60-permutation-sequence/60-permutation-sequence.py
@@ -1,16 +1,13 @@
 import math
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        numbers = []
-        factorials = [1] * n
-        nums = list(map(str,range(1,10)))
+        nums = list(map(str, range(1, 10)))
         res = []
         k -= 1
+        
         while n > 0:
             n -= 1
-            # get the index of current digit
             index, k = divmod(k, math.factorial(n))
             res.append(nums[index])
             nums.remove(nums[index])
-
         return "".join(res)
