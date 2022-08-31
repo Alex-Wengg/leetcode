1b326ac96f9fd62e8a0b1bd29af9cda8b8d15324
H <36247722+Han-Weng@users.noreply.github.com>
2022-08-30 17:41:33 -0700

    Time: 268 ms (96.98%), Space: 15.2 MB (55.27%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.py
+++ 85-maximal-rectangle/85-maximal-rectangle.py
@@ -7,10 +7,12 @@ class Solution:
             stack = [-1]
             for i in range(len(matrix[0])):
                 height[i] = height[i] + 1 if row[i] == '1' else 0
-            
+            # how come col doesn't get out of bound
+                # bc height[] length is + 1 and height[-1] will be 0
             for col in range(len(matrix[0]) + 1):
-                
-                while  height[stack[-1]] > height[col]:
+                # why no check if stack exist
+                    # first loop this while loop will fail bc height[-1]=0
+                while stack and height[stack[-1]] > height[col]:
                     h = height[stack.pop()]
                     w = col - stack[-1] - 1
                     ans = max(ans, h * w)
