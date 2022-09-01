8d1a94c73adb7f229ccff606a536e6f20c982eb6
H <36247722+Han-Weng@users.noreply.github.com>
2022-08-31 20:36:29 -0700

    Time: 103 ms (60.50%), Space: 14.1 MB (93.35%) - LeetHub

--- 87-scramble-string/87-scramble-string.py
+++ 87-scramble-string/87-scramble-string.py
@@ -3,7 +3,7 @@ class Solution:
     
         def scramble( s1, s2):
             n, m = len(s1), len(s2)
-            if n != m or sorted(s1) != sorted(s2):
+            if  sorted(s1) != sorted(s2):
                 return False
             if s1 == s2:
                 return True
@@ -12,9 +12,13 @@ class Solution:
                 return dp[(s1,s2)]
             dp[(s1,s2)] = False
             for i in range(1, n):
-                if f(s1[:i], s2[:i]) and f(s1[i:], s2[i:]) or f(s1[:i], s2[-i:]) and f(s1[i:], s2[:-i]):
+                # simulate the string splitting
+                if f(s1[:i], s2[:i]) and f(s1[i:], s2[i:]) or\
+                f(s1[:i], s2[-i:]) and f(s1[i:], s2[:-i]):
                     dp[(s1, s2)] = True
                     return True
             return dp[(s1,s2)]
         dp = dict()
+
+
         return scramble(s1,s2)
