db904918c4d577c0aa6045a10dbeef889b16b711
H <36247722+Han-Weng@users.noreply.github.com>
2022-08-31 17:03:21 -0700

    Time: 66 ms (87.09%), Space: 14 MB (93.35%) - LeetHub

--- 87-scramble-string/87-scramble-string.py
+++ 87-scramble-string/87-scramble-string.py
@@ -1,21 +1,23 @@
 class Solution:
-    def isScramble(self, s1: str, s2: str) -> bool:
-        def recur(s1, s2):
+    def isScramble(self, s1, s2):
+    
+        def scramble( s1, s2):
+            n, m = len(s1), len(s2)
+            if n != m or sorted(s1) != sorted(s2):
+                return False
             if s1 == s2:
                 return True
-            if Counter(s1) != Counter(s2):
-                return False
+            f = scramble
             if (s1, s2) in dp:
-                return dp[(s1, s2)]
-            dp[(s1, s2)] = False
-            for i in range(1, len(s1)):
-                if recur(s1[:i], s2[:i]) and recur(s1[i:], s2[i:]):
+                return dp[(s1,s2)]
+            dp[(s1,s2)] = False
+            for i in range(1, n):
+                if f(s1[:i], s2[:i]) and f(s1[i:], s2[i:]):
                     dp[(s1, s2)] = True
                     return True
-                if recur(s1[:i], s2[-i:]) and recur(s1[i:], s2[:-i]):
+                if f(s1[:i], s2[-i:]) and f(s1[i:], s2[:-i]):
                     dp[(s1, s2)] = True
                     return True
-            return dp[(s1, s2)]
-        
+            return dp[(s1,s2)]
         dp = dict()
-        return recur(s1, s2)
+        return scramble(s1,s2)
