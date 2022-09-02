17f5866c4cf1f1e89df7cd4b23490b2494c2dc7a
H <36247722+Han-Weng@users.noreply.github.com>
2022-09-01 19:41:01 -0700

    Time: 1608 ms (61.14%), Space: 42.3 MB (44.76%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
@@ -5,7 +5,7 @@ class Solution:
         isPalin = [[False] * n for _ in range(n)]
         for i in range(n):
             for j in range(i+1):
-                if s[i] == s[j] and ( j + 1 >= i or isPalin[i-1][j+1]):
+                if s[i] == s[j] and ( i-j <= 1 or isPalin[i-1][j+1]):
                     isPalin[i][j] = True
                     if j == 0:
                         cut[i] = 0
