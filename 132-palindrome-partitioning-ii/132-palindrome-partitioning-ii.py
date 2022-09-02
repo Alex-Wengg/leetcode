31d22b38bc07ce90e08c0672a7bbf72a1b3c6a40
H <36247722+Han-Weng@users.noreply.github.com>
2022-09-01 19:51:11 -0700

    Time: 881 ms (77.78%), Space: 42.4 MB (44.76%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
@@ -1,15 +1,16 @@
 class Solution:
     def minCut(self, s: str) -> int:
         n = len(s)
-        cut =   [n] * n 
-        isPalin = [[False] * n for _ in range(n)]
-        for i in range(n):
+        cut = [n] * n 
+        isPalin = [[False] * len(s) for i in range(len(s))]
+        
+        for i in range(len(s)):
             for j in range(i+1):
-                if s[i] == s[j] and ( i-j <= 1 or isPalin[i-1][j+1]):
+                
+                if s[i] == s[j] and (i - j < 2 or isPalin[i-1][j+1]):
                     isPalin[i][j] = True
                     if j == 0:
                         cut[i] = 0
-                    else:
-                        cut[i] = min(cut[i], cut[j-1]+1)
-                    
+                    else:   
+                        cut[i] = min(cut[i], cut[j-1] + 1)    
         return cut[-1]
