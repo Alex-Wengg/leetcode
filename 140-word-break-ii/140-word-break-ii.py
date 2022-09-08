c5457cb177d1a34551e5692b36acfdaac54e13e1
H <36247722+Han-Weng@users.noreply.github.com>
2022-09-07 19:56:05 -0700

    Time: 35 ms (85.52%), Space: 14 MB (33.70%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.py
+++ 140-word-break-ii/140-word-break-ii.py
@@ -8,16 +8,16 @@ class Solution:
             for end in range(start+1, n+1):
                 if dp_sol[start] and s[start:end] in wordSet:
                     dp_sol[end] = True
-        # print(dp_sol)
+
         if not dp_sol[-1]:
             return []
         
-        dp = [[""]] + [[] for _ in range(n)] 
+        dp = [[] for _ in range(n+1)]
+        dp[0] = [""]
         for start in range(n):
             for end in range(start+1, n+1):
                 if s[start:end] in wordSet:
                     for sub in dp[start]:
-                        dp[end].append(sub+" "+s[start:end])
-
+                        dp[end].append(sub + " " + s[start:end])
         return [s[1:] for s in dp[-1]]
         
