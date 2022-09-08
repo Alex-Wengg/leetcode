98f5647cbdef5d5ba82caef6e4371572ffdbd4f2
H <36247722+Han-Weng@users.noreply.github.com>
2022-09-07 19:36:52 -0700

    Time: 32 ms (91.97%), Space: 13.9 MB (81.96%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.py
+++ 140-word-break-ii/140-word-break-ii.py
@@ -1,22 +1,23 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         
-        return self.helper(s, wordDict, {})
-    
-    def helper(self, s, wordDict, memo):
-        if s in memo: return memo[s] 
-        if not s: return []
-    
-        res = []
-        for word in wordDict:
-            if not s.startswith(word):
-                continue
-            if len(s) == len(word):
-                res.append(word)
-            else:
-                restOfResult = self.helper(s[len(word):], wordDict, memo)
-                for item in restOfResult:
-                    item = word + ' ' + item
-                    res.append(item)
-        memo[s] = res
-        return res
+        wordSet = set(wordDict)
+        n = len(s)
+        dp_sol = [True] + [False] * n 
+        for start in range(n):
+            for end in range(start+1, n+1):
+                if dp_sol[start] and s[start:end] in wordSet:
+                    dp_sol[end] = True
+        # print(dp_sol)
+        if not dp_sol[-1]:
+            return []
+        
+        dp = [[""]] + [[] for _ in range(n)] 
+        for start in range(n):
+            for end in range(start+1, n+1):
+                if s[start:end] in wordSet:
+                    for sub in dp[start]:
+                        dp[end].append(sub+" "+s[start:end])
+
+        return [s[1:] for s in dp[-1]]
+        
