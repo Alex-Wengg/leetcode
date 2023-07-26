b4121bb0cb96a12b8d0b104da0e2e955e1b45024
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-07-26 14:01:29 -0400

    Time: 190 ms (63.76%), Space: 17.3 MB (18.72%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.py
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.py
@@ -1,27 +1,42 @@
 class Solution:
     def removeInvalidParentheses(self, s):
-        ans =[]
-        def remove(s, ans, last_i, last_j, par):
-            stack = 0
-            for i in range(last_i, len(s)):
-                if s[i] == par[0]:
-                    stack += 1
-                if s[i] == par[1]:
-                    stack -= 1
-                if stack >= 0:
-                    continue 
+        res = []
+        if not s:
+            return res
 
-                for j in range(last_j, i+1):
-                    if s[j] == par[1] and (j == last_j or s[j-1] != par[1]):
-                        remove(s[0:j] + s[j+1: len(s)], ans, i,j, par )
-                return 
-            reversed = s[::-1]
-            if par[0] == '(':
-                remove(reversed, ans, 0,0, [')', '('])
-            else:
-                ans.append(reversed)
-                
-        remove(s, ans, 0, 0, ['(',')'])
-        return ans
+        visited = set()
+        queue = []
 
-    
+        visited.add(s)
+        queue.append(s)
+
+        found = False
+
+        def isValided(s):
+            count = 0
+            for i in range(len(s)):
+                if s[i] == '(':
+                    count += 1
+                if s[i] == ')' and count == 0:
+                    return False
+                elif s[i] == ')':
+                    count -= 1
+            return count == 0 
+
+        while queue:
+            s = queue.pop(0)
+            if isValided(s):
+                found = True
+                res.append(s)
+            if found:
+                continue
+            
+            for i in range(len(s)):
+                if s[i] not in {'(',')'}:
+                    continue
+                t = s[0:i] + s[i+1:]
+                if t not in visited:
+                    queue.append(t)
+                    visited.add(t)
+            
+        return res
