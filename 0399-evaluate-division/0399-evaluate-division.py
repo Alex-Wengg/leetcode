64d78a398fd3c9fe2ab521f58f1d4a58754ae1cc
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-09-09 18:59:16 -0400

    Time: 38 ms (76.8%), Space: 16.5 MB (36%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,36 +1,36 @@
+from collections import defaultdict
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        # Step 1. Build the Graph
-        graph = collections.defaultdict(dict)
-        for (x, y), val in zip(equations,values):
-            graph[x][y] = val
-            graph[y][x] = 1.0 / val
-        print(graph)
-        
-        # Step 2. DFS function
-        def dfs(x, y, visited):
-            # neither x not y exists
+        graph = defaultdict(list) # string, [](string, double)
+
+        results = []
+
+        for (x, y), z in zip(equations, values):
+            graph[x].append([y, z])
+            graph[y].append([x, 1.0/z])
+        # ================================
+        def dfs( start, end, visited):
+            if start == end:
+                return 1.0
+            visited.add(start)
+            temp = -1.0
+            for mid, sec in graph[start]:
+                if mid in visited:
+                    continue
+                temp2 = dfs(mid, end, visited)
+                if temp2 != -1.0:
+                    temp = sec * temp2
+                    break
+            return temp
+
+        for x,y in queries:
             if x not in graph or y not in graph:
-                return -1.0
-            
-            # x points to y
-            if y in graph[x]:
-                return graph[x][y]
-            
-            # x maybe connected to y through other nodes
-            # use dfs to see if there is a path from x to y
-            for i in graph[x]:
-                if i not in visited:
-                    visited.add(i)
-                    temp = dfs(i, y, visited)
-                    if temp == -1:
-                        continue
-                    else:
-                        return graph[x][i] * temp
-            return -1
-            
-        # go through each of the queries and find the value
-        res = []
-        for query in queries:
-            res.append(dfs(query[0], query[1], set()))
-        return res
+                results.append(-1.0)
+            elif x == y:
+                results.append(1.0)
+            else:
+                visited = set()
+                temp = dfs(x, y, visited)
+                results.append(temp)
+        
+        return results
