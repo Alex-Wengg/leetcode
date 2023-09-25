fec2a38793ca4066b25ad7861f7d146f31d7254b
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-09-25 19:31:21 -0400

    Time: 797 ms (85.4%), Space: 39.6 MB (67.73%) - LeetHub

--- 3113-beautiful-towers-ii/3113-beautiful-towers-ii.py
+++ 3113-beautiful-towers-ii/3113-beautiful-towers-ii.py
@@ -1,29 +1,14 @@
 class Solution:
     def maximumSumOfHeights(self, A: List[int]) -> int:
         n = len(A)
-
-        left = [0] * n 
-        stack = [-1]
-        cur = 0
-
-        for i in range(n):
-            while len(stack) > 1 and A[stack[-1]] > A[i]: 
-                j = stack.pop() 
-                cur -= (j - stack[-1]) * A[j]
-            cur += (i - stack[-1]) * A[i]
-            stack.append(i)
-            left[i] = cur 
-
-        stack = [n] 
-        res = 0 
-        cur = 0
-
-        for i in range(n - 1, -1, -1):
-            while len(stack) > 1 and A[stack[-1]] > A[i]:
-                j = stack.pop()
-                cur -= -(j - stack[-1]) * A[j]
-            cur += -(i - stack[-1]) * A[i]
-            stack.append(i)
-            res = max(res, left[i] + cur - A[i])
+        def f(A):
+            res = [0] * n
+            stack = [-1]
+            for i in range(n):
+                while len(stack) > 1 and A[stack[-1]] > A[i]:
+                    j = stack.pop()
+                res[i] = res[stack[-1]] + (i - stack[-1]) * A[i]
+                stack.append(i)
+            return res
         
-        return res
+        return max(pre + suf - a for pre,suf,a in zip(f(A), f(A[::-1])[::-1], A))
