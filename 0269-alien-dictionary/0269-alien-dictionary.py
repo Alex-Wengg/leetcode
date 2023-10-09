472613c78e086b95ca9e85de57c0f9820b92909e
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-10-09 14:09:49 -0400

    Time: 45 ms (34.03%), Space: 15.7 MB (100%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.py
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -1,43 +1,54 @@
 class Solution:
   def alienOrder(self, words):
-    def dfs(i):
-      seen[i] = 0 
-      for nei in graph[i]:
-        if nei in seen:
-          if seen[nei] == 0:
-            return False
-        else:
-          if not dfs(nei):
-            return False
+    map = {}
+    degree = {}
+    result = ""
+
+    if (not(words)) :
+      return result
+
+    for s in words:
+      for c in s:
+        degree[c] = 0
+
+    for i in range(len(words)-1):
+      cur = words[i]
+      next = words[i+1] 
+      if len(cur) > len(next) and cur.startswith(next):
+        return ""
+      length = min(len(cur), len(next))
+
+      for j in range(length):
+        c1 = cur[j]
+        c2 = next[j]
+        if c1 != c2:
+          set_ = set()
+          if (c1 in map):
+            set_ = map[c1]
+          
+          if c2 not in set_:
+            set_.add(c2)
+            map[c1] = set_
+            degree[c2] = degree.get(c2, 0) + 1 
+          
+          break
       
-      seen[i] = 1 
-      res.appendleft(i)
-      return True 
-  
-    nodes = set()
-    for word in words:
-        nodes |= set(word)
-
-    graph = collections.defaultdict(set)
-    for i in range(len(words) -1 ):
-      k = 0 
-      diff = False
-
-      while k < len(words[i]) and k < len(words[i+1]):
-        if words[i][k] != words[i+1][k]:
-          graph[words[i][k]].add(words[i+1][k])
-          diff = True
-
-          break 
-        else:
-          k += 1
-      if not diff and len(words[i]) > len(words[i+1]):
-          return ""
+    q = []
+    for c, v in degree.items():
+      if v == 0:
+        q.append(c)
       
-    res = collections.deque()
-    seen = {}
-    for i in nodes:
-        if i not in seen:
-            if not dfs(i):
-                return ""
-    return "".join(res)
+    while q:
+      c = q.pop()
+      result += c
+      if c in map:
+        for c2 in map[c]:
+          degree[c2] = degree[c2] -1
+          if degree[c2] == 0:
+            q.append(c2)
+      
+    if len(result) != len(degree):
+      return ""
+    return result 
+
+
