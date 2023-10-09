a513e8b00b10400420f89cadbf22a736a5aa0e63
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-10-09 18:52:41 -0400

    Time: 39 ms (75.42%), Space: 16.2 MB (82.69%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.py
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -2,11 +2,13 @@ class Solution:
     def alienOrder(self, words):
         # a -> b
         adj = defaultdict(set)
-        # in-degree
+        
+        # in-degree construction
         deg = {c: 0 for w in words for c in w}
+
+        # adjList construction
         for i, w1 in enumerate(words[:-1]):
             is_prefix = True
-
             w2 = words[i + 1]
             for c1, c2 in zip(w1, w2):
                 if c1 == c2: continue
@@ -18,9 +20,8 @@ class Solution:
             if is_prefix == True and len(w1) > len(w2):
                 return ""
 
-
         res = ''
-        # start w 0 indegree nodes
+        # sort by indegree, traversed by adjList
         q = deque([c for c in deg if not deg[c]])
         while q:
             c = q.popleft()
@@ -28,4 +29,4 @@ class Solution:
             for n in adj[c]:
                 deg[n] -= 1
                 if not deg[n]: q.append(n)
-        return res if len(res) == len(deg) else ''           
+        return res if len(res) == len(deg) else ''    
