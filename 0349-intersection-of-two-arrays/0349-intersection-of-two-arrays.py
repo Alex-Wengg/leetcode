a7f1fef63321bdbeabea7a3c30de8147df83404e
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-10-09 13:09:57 -0400

    Time: 56 ms (39.65%), Space: 16.2 MB (96.23%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,9 +1,7 @@
 class Solution(object):
     def intersection(self, nums1, nums2):
         res = []
-        map = {}
-        for i in nums1:
-            map[i] = map.get(i, 0) + 1
+        map = {i : 0 for i in nums1}
         for j in nums2:
             if j in map:
                 res.append(j)
