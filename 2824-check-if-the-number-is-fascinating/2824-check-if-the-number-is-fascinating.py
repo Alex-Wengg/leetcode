6f67003436e2102aca681d053afd43b90547c40b
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-10-29 18:34:12 -0400

    Time: 27 ms (99.32%), Space: 16.3 MB (8.68%) - LeetHub

--- 2824-check-if-the-number-is-fascinating/2824-check-if-the-number-is-fascinating.py
+++ 2824-check-if-the-number-is-fascinating/2824-check-if-the-number-is-fascinating.py
@@ -1,6 +1,3 @@
 class Solution:
     def isFascinating(self, n: int) -> bool:
-        seen_ = "123456789"
-
-        check = str(n) + str(2 * n) + str(3 * n) 
-        return seen_ == ''.join(sorted(check))
+        return "123456789" == ''.join(sorted(str(n) + str(2 * n) + str(3 * n) ))
