b6b0f44aed0cdfe1fd5415efa5ec683c241a4168
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-10-30 12:50:40 -0400

    Time: 536 ms (27.13%), Space: 29.4 MB (85.89%) - LeetHub

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -5,14 +5,10 @@ class Solution:
         for i in range(len(arr)  ):
             
             sum_ += arr[i]
-            
-            # Check if the current subarray has size k
-            if i >= k - 1:
-                # If the current subarray's average is greater than or equal to threshold, increase the count
-                if sum_ / k >= threshold:
-                    cnt += 1
-                
-                # Remove the first element of the subarray from the sum
+            cnt += sum_ / k >= threshold and i -k + 1 >= 0
+
+            if i -k + 1 >= 0:
                 sum_ -= arr[i-k+1]
+
         return cnt
 
