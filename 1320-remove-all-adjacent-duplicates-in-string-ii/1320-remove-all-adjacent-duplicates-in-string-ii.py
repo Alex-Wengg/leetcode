9a16eee97543abc8dc7d8c886d363c56746d6d79
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-11-01 17:10:41 -0400

    Time: 97 ms (49.95%), Space: 21 MB (60.82%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.py
@@ -4,13 +4,11 @@ class Solution:
         stack = []
 
         for i in range(0, len(s)):
-
             if not(stack) or stack[-1][0] != s[i]:
                 stack.append([s[i], 1])
-            
             else:
                 stack[-1][1] += 1
                 if stack[-1][1] == k:
                     stack.pop()
         
-        return ''.join(char * count for char, count in stack)
+        return ''.join( char * count for char, count in stack)
