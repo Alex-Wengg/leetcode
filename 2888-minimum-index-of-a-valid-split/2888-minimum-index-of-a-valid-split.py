c962698bef217d81c32090d1dd86b92e01d55f3d
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-11-02 12:58:45 -0400

    Time: 681 ms (17.15%), Space: 31.8 MB (69.71%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.py
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.py
@@ -11,20 +11,12 @@ class Solution:
         dominant = max(suffix_counter, key=suffix_counter.get)
 
         for i in range(n - 1):
-            # Move the current number from suffix to prefix
             prefix_counter[nums[i]] += 1
             suffix_counter[nums[i]] -= 1
             
-            # If this element is no longer in the suffix, remove it
-            if suffix_counter[nums[i]] == 0:
-                del suffix_counter[nums[i]]
-            
-            # Check if the dominant element is the same for the prefix and suffix
-            # And whether it meets the frequency requirements
-            if   prefix_counter[dominant] * 2 > i + 1 and \
-                    suffix_counter[dominant] * 2 > n - i - 1:
-                # Update result to the current index if all conditions are met
+            if  prefix_counter[dominant] * 2 > i + 1 and \
+             suffix_counter[dominant] * 2 > n - i - 1:
                 res = i
-                break  # We can break early since we only need the first valid split
+                break 
 
         return res
