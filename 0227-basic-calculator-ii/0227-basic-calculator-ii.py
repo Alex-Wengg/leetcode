b0d88437dcfe74c94932d5105062775b8ff576d6
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-11-03 18:13:28 -0400

    Time: 131 ms (49.37%), Space: 19.4 MB (61.53%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -7,18 +7,19 @@ class Solution:
         for i, c in enumerate(s):
             if c.isdigit():
                 num = num * 10 + int(c)
-            if c in "-+*/" or (  i == len(s) - 1):
-
-                if sign in "-+":
-                    stack.append(num if sign == '+' else -num)
-                elif sign == "*":
-                    stack.append(stack.pop()*num)
+            if c in '-+*/' or i == len(s) - 1:
+                if sign in '-+':
+                    stack.append( num if sign == '+' else -num)
+                elif sign in '*':
+                    stack[-1] *= num
                 else:
+                    
                     tmp = stack.pop()
-                    if tmp//num < 0 and tmp%num != 0:
-                        stack.append(tmp//num+1)
+                    if tmp // num < 0 and tmp % num > 0:
+                        stack.append(tmp // num + 1)
                     else:
-                        stack.append(tmp//num)
+                        stack.append(tmp // num)
                 sign = c
                 num = 0
-        return sum(stack)
+
+        return sum(stack) 
