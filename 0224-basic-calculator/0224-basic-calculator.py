eed8012523fe9d3092bfea12b779bb2d6c9d52cd
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-11-03 17:18:41 -0400

    Time: 74 ms (63.31%), Space: 17.5 MB (92.89%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -8,23 +8,18 @@ class Solution:
         for i, c in enumerate(s):
             if c.isdigit():
                 number = 10 * number + int(c)
-            elif c == '+':
+            elif c in '-+)':
                 result += sign * number
                 number = 0
-                sign = 1
-            elif c == '-':
-                result += sign * number
-                number = 0
-                sign = -1
+                if c in '-+':
+                    sign = 1 if c == '+' else -1
+                elif c == ')':
+                    result *= stack.pop()
+                    result += stack.pop()
             elif c == '(':
                 stack.extend([ result, sign ])
                 sign = 1
                 result = 0
-            elif c == ')':
-                result += sign * number 
-                number = 0
-                result *= stack.pop()
-                result += stack.pop()
             
         if number != 0:
             result += sign * number 
