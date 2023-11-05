45bc5ca73f621e1da2ace03e70a2e5b4624e2e4b
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-11-05 18:45:42 -0500

    Time: 1800 ms (95.22%), Space: 56.6 MB (49.57%) - LeetHub

--- 1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/1701-remove-max-number-of-edges-to-keep-graph-fully-traversable.py
+++ 1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/1701-remove-max-number-of-edges-to-keep-graph-fully-traversable.py
@@ -12,34 +12,34 @@ class Solution:
             # return parent[i] if i == parent[i] else find(parent[i])
         
         def union(x, y):
-            x, y = find(x), find(y)
-            if y == x:
-                return 0
+            x,y = find(x), find(y)
+            if x== y:
+                return 0 
             parent[x] = y
             return 1
-
-
+        
         for t, i, j in edges:
             if t == 3:
-                if union(i, j):
+                if union(i ,j):
                     e1 += 1
                     e2 += 1
                 else:
                     res += 1
+        
         root0 = parent[:]
-
         for t, i, j in edges:
-            if t == 1:
-                if union(i, j):
+            if t == 2:
+                if union(i,j):
                     e1 += 1
                 else:
                     res += 1
         
         parent = root0
-        for t, i, j in edges:
-            if t == 2:
+        for t,i,j in edges:
+            if t == 1:
                 if union(i, j):
                     e2 += 1
                 else:
                     res += 1
-        return res if e1 == e2 == n -1 else - 1
+        
+        return res if e1 == e2 == n -1 else -1
