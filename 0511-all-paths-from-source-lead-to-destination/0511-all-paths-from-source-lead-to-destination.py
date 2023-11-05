e1615b870aab5b6ccab1cc6f395d2c6515510266
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-11-05 12:13:03 -0500

    Time: 298 ms (12.12%), Space: 24 MB (9.27%) - LeetHub

--- 0511-all-paths-from-source-lead-to-destination/0511-all-paths-from-source-lead-to-destination.py
+++ 0511-all-paths-from-source-lead-to-destination/0511-all-paths-from-source-lead-to-destination.py
@@ -1,22 +1,24 @@
 class Solution:
     def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         graph = collections.defaultdict(set)
-        for u, v in edges:
-            graph[u].add(v)
-        visited = [0 for _ in range(n)]
+
+        for a, b in edges:
+            graph[a].add(b)
+            
+        visited = [ 0 for _ in range(n)]
+
         def dfs(i):
             if len(graph[i]) == 0:
                 return i == destination
-            elif visited[i] == 1:
-                return False
-            elif visited[i] == 2:
-                return True
+            elif visited[i] in  [1, 2]:
+                return visited[i] == 2
             else:
                 visited[i] = 1
-                for j in graph[i]:
-                    if not dfs(j):
+                for nei in graph[i]:
+                    if not dfs(nei):
                         return False
+                
                 visited[i] = 2
-                return True    
-
+                return True 
+            
         return dfs(source)
