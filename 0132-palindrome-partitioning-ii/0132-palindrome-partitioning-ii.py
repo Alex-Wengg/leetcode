6c912c5fa882fd5214df131b78deabc5eb7c3392
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-11-06 14:31:36 -0500

    Time: 1121 ms (59.32%), Space: 47.8 MB (32.81%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
@@ -7,13 +7,10 @@ class Solution:
 
         for i in reversed(range(n)):
             for j in range(i, n):
-
                 if s[i] == s[j] and ( j - i < 3 or dp[i+1][j-1]):
                     dp[i][j] = 1
-                    if n - 1 == j:
-                        cuts[i] = 0
-                    else:
-                        cuts[i] = min(cuts[i], cuts[j+1] + 1) # shouldn't dp cut check if not mirror
+           
+                    cuts[i] = min(cuts[i], cuts[j+1] + 1) if n - 1 != j else 0# shouldn't dp cut check if not mirror
         
         return cuts[0]
 
