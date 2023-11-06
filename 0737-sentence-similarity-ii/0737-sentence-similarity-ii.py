cf20e29b3d096661585fe4c8df87c12931bacdd7
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-11-06 12:56:24 -0500

    Time: 466 ms (50.41%), Space: 17.8 MB (89.43%) - LeetHub

--- 0737-sentence-similarity-ii/0737-sentence-similarity-ii.py
+++ 0737-sentence-similarity-ii/0737-sentence-similarity-ii.py
@@ -1,26 +1,27 @@
 class Solution:
     def areSentencesSimilarTwo(self, words1: List[str], words2: List[str], pairs: List[List[str]]) -> bool:
         
-        if len(words1) != len(words2):
+        parent = {}
+
+        def find(w):
+            if w not in parent:
+                parent[w] = w
+            while w in parent and parent[w] != w:
+                w = parent[w]
+            return w
+        
+        for a, b in pairs:
+            a1 = find(a)
+            b1 = find(b)
+            parent[a1] = b1
+        
+        l1 = len(words1)
+        l2 = len(words2)
+        if l1 != l2:
             return False
         
-        connections = defaultdict(set)
-        for w1, w2 in pairs:
-            connections[w1].add(w2)
-            connections[w2].add(w1)
-        
-        roots = {}
-        def dfs(word, root_word):
-            if word in roots:
-                return None
-            roots[word] = root_word
-            for connectedWord in connections[word]:
-                dfs(connectedWord, root_word)
-        
-        for word in connections:
-            dfs(word, word)
-        
-        for w1,w2 in zip(words1, words2):
-            if roots.get(w1, w1) != roots.get(w2,w2):
+        for i in range(l1):
+            a, b = find(words1[i]), find(words2[i])
+            if a != b:
                 return False
-        return True 
+        return True
