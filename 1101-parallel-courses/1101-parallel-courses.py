7e2baf0a2f9b76600cf77ffee249782f0d2325d4
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-11-06 09:55:52 -0500

    Time: 239 ms (89.84%), Space: 20.3 MB (29.3%) - LeetHub

--- 1101-parallel-courses/1101-parallel-courses.py
+++ 1101-parallel-courses/1101-parallel-courses.py
@@ -1,7 +1,12 @@
 class Solution:
     def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
         
-        def dfs(i, g, vDepth, visitState):
+
+
+        vDepth = [1] * n
+        visitState = [0] * n
+        g = [[] for i in range(n)]
+        def dfs(i):
             if visitState[i]  == 1:
                 return True
             if visitState[i] == -1:
@@ -9,19 +14,16 @@ class Solution:
             
             visitState[i] = -1
             for j in g[i]:
-                if not(dfs(j, g, vDepth, visitState)):
+                if not(dfs(j)):
                     return False
                 vDepth[i] = max(vDepth[i], 1 + vDepth[j])
             visitState[i] = 1
             return True 
-
-        vDepth = [1] * n
-        visitState = [0] * n
-        g = [[] for i in range(n)]
+        
         for v in relations:
             g[v[0] - 1].append(v[1] - 1)
 
         for i in range(n):
-            if not(dfs(i, g, vDepth, visitState)):
+            if not(dfs(i)):
                 return -1
         return max(vDepth)
