8a63afcab849f0e66ed84d08b520aef00266e48b
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-11-06 18:21:52 -0500

    Time: 124 ms (92.59%), Space: 23.9 MB (59.26%) - LeetHub

--- 1220-smallest-sufficient-team/1220-smallest-sufficient-team.py
+++ 1220-smallest-sufficient-team/1220-smallest-sufficient-team.py
@@ -1,19 +1,21 @@
 class Solution:
     def smallestSufficientTeam(self, req_skills, people):
-        skill_index = {v: i for i, v in enumerate(req_skills)}
-        dp = {0: []}
+        skill_index = { v: i for i, v in enumerate(req_skills)}
+        dp = {0 : []}
 
         for i, p in enumerate(people):
-            cur_skill = 0
+            curr_skill = 0
             for skill in p:
-                cur_skill |= 1 << skill_index[skill] if skill in skill_index else 0
-
+                curr_skill |= 1 << skill_index.get(skill, 0 )
+            
             for prev, need in dict(dp).items():
-                comb = cur_skill | prev
+                comb = prev | curr_skill
                 if comb == prev:
-                    continue 
+                    continue
                 if comb not in dp or len(dp[comb]) > len(need) + 1:
                     dp[comb] = need + [i]
-             
-        return dp[(1 <<  len(req_skills)) - 1]       
-      
+            
+        return dp[(1 << len(req_skills)) - 1]
+
+
+
