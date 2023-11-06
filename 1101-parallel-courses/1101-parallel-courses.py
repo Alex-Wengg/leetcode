cbddbc014253fe0f3fe81b68177d0246ad90ee8c
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-11-06 10:07:09 -0500

    Time: 271 ms (10.96%), Space: 19.8 MB (89.61%) - LeetHub

--- 1101-parallel-courses/1101-parallel-courses.py
+++ 1101-parallel-courses/1101-parallel-courses.py
@@ -1,27 +1,25 @@
 class Solution:
     def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
-        
+        indegree = [0] * n
+        g = [ set() for i in range(n)]
 
-
-        vDepth = [1] * n
-        visitState = [0] * n
-        g = [[] for i in range(n)]
-        def dfs(i):
-            if visitState[i] != 0 :
-                return visitState[i] == 1
-            
-            visitState[i] = -1
-            for j in g[i]:
-                if not(dfs(j)):
-                    return False
-                vDepth[i] = max(vDepth[i], 1 + vDepth[j])
-            visitState[i] = 1
-            return True 
-        
         for v in relations:
-            g[v[0] - 1].append(v[1] - 1)
-
+            g[v[0] - 1].add(v[1] - 1)
+            indegree[v[1] - 1] += 1
+        
+        maxDepth = 0
+        numStudied = 0
+        q = []
         for i in range(n):
-            if not(dfs(i)):
-                return -1
-        return max(vDepth)
+            if indegree[i] == 0:
+                q.append(( i, 1 ))
+        
+        while q:
+            numStudied += 1
+            i, depth = q.pop(0)
+            maxDepth = max(depth, maxDepth)
+            for j in g[i]:
+                indegree[j] -= 1
+                if indegree[j] == 0:
+                    q.append((j, depth + 1))
+        return maxDepth if numStudied == n else - 1
