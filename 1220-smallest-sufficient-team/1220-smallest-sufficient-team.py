10dd2825854ad8a80c1721f112725165347b244d
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-11-06 21:09:55 -0500

    Time: 143 ms (87.04%), Space: 23.9 MB (59.26%) - LeetHub

--- 1220-smallest-sufficient-team/1220-smallest-sufficient-team.py
+++ 1220-smallest-sufficient-team/1220-smallest-sufficient-team.py
@@ -1,17 +1,19 @@
 class Solution:
     def smallestSufficientTeam(self, req_skills, people):
-        skill_index = { v : i for i, v in enumerate(req_skills)}
-        dp = { 0 : []}
 
-        for i, person in enumerate(people):
-            curr_skill = 0 
-            for skill in person:
-                curr_skill |= 1 << skill_index[skill] if skill in skill_index else 0
+        skill_index = { v : i for i,v in enumerate(req_skills)}
+        dp = { 0 : [] }
+
+        for i, individal_skills in enumerate(people):
+            curr_skill = 0
+            for skill in individal_skills:
+                curr_skill |= (1 << skill_index[skill] if skill in skill_index else 0 )
             
-            for prev, need in dict(dp).items():
-                comb = prev | curr_skill 
-                if comb not in dp or len(dp[comb]) > len(need) + 1:
-                    dp[comb] = need + [i]
-
-        return dp[ (1 << len(req_skills)) - 1]
-
+            for prev, needed in dict(dp).items():
+                comb = prev | curr_skill
+                if comb == prev:
+                    continue
+                if comb not in dp or len(dp[comb]) > len(needed) + 1:
+                    dp[comb] = needed + [i]
+            
+        return dp[(1 << len(req_skills)) - 1]
