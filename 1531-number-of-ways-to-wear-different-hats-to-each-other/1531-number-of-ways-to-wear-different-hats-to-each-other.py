86ce180ce5607cf70acfbb9b1160a0bc7fc596ba
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-11-06 23:14:22 -0500

    Time: 205 ms (92.73%), Space: 16.3 MB (95.45%) - LeetHub

--- 1531-number-of-ways-to-wear-different-hats-to-each-other/1531-number-of-ways-to-wear-different-hats-to-each-other.py
+++ 1531-number-of-ways-to-wear-different-hats-to-each-other/1531-number-of-ways-to-wear-different-hats-to-each-other.py
@@ -2,23 +2,22 @@ class Solution:
     def numberWays(self, hats: List[List[int]]) -> int:
         
 
-        persons = [ [] for i in range(40)]
+        h2p = [ [] for i in range(40)]
         n = len(hats)
         mod = 10 ** 9 + 7
 
-        masks = [ 0 for i in range(1 << n)]
-        masks[0] = 1
+        dpPersonHatsAssigned = [ 0 for i in range(1 << n)]
+        dpPersonHatsAssigned[0] = 1
 
         for i in range(n):
             for h in hats[i]:
-                persons[h-1].append(i)
+                h2p[h-1].append(i)
         
         for i in range(40): # hats
             for j in range((1 << n) - 1, -1, -1): # people
 
-                for p in persons[i]: 
+                for p in h2p[i]: # assign person_y to hat_x
                     if ((j & ( 1 << p)) == 0): # not assigned a hat yet
-                        masks[ j | ( 1 << p)] += masks[j]
-                        masks[ j | ( 1 << p)] %= mod
+                        dpPersonHatsAssigned[ j | ( 1 << p)] += dpPersonHatsAssigned[j] 
         
-        return masks[-1]
+        return dpPersonHatsAssigned[-1]% mod
