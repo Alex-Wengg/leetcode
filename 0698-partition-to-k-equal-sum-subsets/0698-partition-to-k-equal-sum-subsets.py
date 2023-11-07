23a068a081e14bcfec77291d06b1aa89527be4d2
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-11-06 22:54:54 -0500

    Time: 2353 ms (9.49%), Space: 19.3 MB (35.52%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -1,44 +1,19 @@
 class Solution:
-    def canPartitionKSubsets(self, nums, k):
-        if not nums:
+    def canPartitionKSubsets(self, arr: List[int], k: int) -> bool:
+        n = len(arr)
+        dp = [ -1 for i in range(1<< 16 + 2)]
+        dp[0] = 0
+
+        sum_ = sum(arr)
+        if sum_ % k:
             return False
+        tar = sum_ // k 
 
-        n = len(nums)
-        # Result array
-        dp = [False] * (1 << n)
-        total = [0] * (1 << n)
-        dp[0] = True
-
-        # Calculate sum of all array elements
-        total_sum = sum(nums)
-        # Sort the array
-        nums.sort()
-
-        # If total_sum is not divisible by k, we can't partition the array
-        if total_sum % k != 0:
-            return False
-        target_sum = total_sum // k
-        # If the largest number is greater than the target sum, we can't partition
-        if nums[-1] > target_sum:
-            return False
-
-        # Loop over power set
-        for i in range(1 << n):
-            if dp[i]:
-                # Loop over each element to find subset
-                for j in range(n):
-                    # Set the j-th bit
-                    temp = i | (1 << j)
-                    if temp != i:
-                        # If total sum is less than target store in dp and total array
-                        if nums[j] <= (target_sum - (total[i] % target_sum)):
-                            dp[temp] = True
-                            total[temp] = nums[j] + total[i]
-                        else:
-                            break
-
-        return dp[(1 << n) - 1]
-
-    # Example usage:
-    # print(can_partition_k_subsets([4, 3, 2, 3, 5, 2, 1], 4))  # True or False depending on the possibility
-
+        for mask in range(1 << n):
+            if dp[mask] == -1:
+                continue
+            for i in range(n):
+                if (not(mask & 1 << i) and dp[mask] + arr[i] <= tar):
+                    dp[mask | (1 << i)] = (dp[mask] + arr[i]) % tar
+        
+        return dp[(1 << n) -1 ] == 0
