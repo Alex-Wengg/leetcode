18a7c121f80d3ea4906c286be3731200e26ce150
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-11-07 16:35:07 -0500

    Time: 212 ms (91.74%), Space: 16.4 MB (90.83%) - LeetHub

--- 1531-number-of-ways-to-wear-different-hats-to-each-other/1531-number-of-ways-to-wear-different-hats-to-each-other.py
+++ 1531-number-of-ways-to-wear-different-hats-to-each-other/1531-number-of-ways-to-wear-different-hats-to-each-other.py
@@ -1,20 +1,18 @@
 class Solution:
     def numberWays(self, p2h: List[List[int]]) -> int:
-
-        h2p = [ [] for i in range(40)] # 40^10 faster > 10^40
         n = len(p2h)
-        dp = [0] * (1 << n)
+        h2p = [ [] for i in range(40)]
+        dp = [0 for i in range(1<<n)] # represents ways to give specific ppl hats
         dp[0] = 1
 
         for i in range(n):
             for h in p2h[i]:
-                h2p[h-1].append(i)    
+                h2p[h - 1].append(i)
         
-        for i in range(40):
-            for j in range((1 << n) -1, -1, -1): # we want to go from everyone has hat to no one has hat
-                print(bin(j))
-                for p in h2p[i]: # people assigned to hat_i
-                    if (j & 1 << p) == 0:
-                        dp[j | 1 << p] += dp[j]
+        for i in range(40): # loop each hat 
+            for j in range((1<<n)-1, -1, -1): # loop each person combination, who has hat(1) / nohat(0)
+                for p in h2p[i]: # for person in hat : personList
+                    if (j & (1<<p)) == 0: # person_p no hat
+                        dp[j | 1<<p ] += dp[j] # 
         
         return dp[-1] % (10**9 + 7)
